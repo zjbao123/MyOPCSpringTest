bbea27645b5d8408d0bf22f3937c7b903c6355f9
sukun bao <345232213@qq.com>
2017-04-05 11:15:59 +0800

    数据库连接成功

--- src/main/java/com/opc/controller/NewWorld.java
+++ src/main/java/com/opc/controller/NewWorld.java
@@ -1,6 +1,8 @@
 package com.opc.controller;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by zjbao on 2017/4/4.
  */
-@RestController
+@Controller
 @EnableAutoConfiguration
 
 public class NewWorld {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
-        return "Hello World~";
+        return "index";
     }
 }
 
