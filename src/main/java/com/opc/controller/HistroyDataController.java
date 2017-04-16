82ddcbc7ed68c50f8ee45192201e551d44e0de0a
sukun bao <345232213@qq.com>
2017-04-16 21:31:19 +0800

    历史数据动态图 get√

--- src/main/java/com/opc/controller/HistroyDataController.java
+++ src/main/java/com/opc/controller/HistroyDataController.java
@@ -23,7 +23,7 @@ public class HistroyDataController {
 
     @RequestMapping(value = "/historyDataSource")
     public List<Data> getHistoryUserList(){
-        Pageable pageable = new PageRequest(0, 100, Sort.Direction.DESC, "TIME");
+        Pageable pageable = new PageRequest(0, 10000, Sort.Direction.DESC, "TIME");
         Page<Data> DataPage = DataRepository.findLastData(pageable);
         return DataPage.getContent();
     }
